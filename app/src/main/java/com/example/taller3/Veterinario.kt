92f06aac709142e9c31f0f3824accde379719638
MarcoAValencia <60183467+MarcoAValencia@users.noreply.github.com>
2023-05-26 10:08:23 -0500

    Add veterinario data class

---
+++ app/src/main/java/com/example/taller3/Veterinario.kt
@@ -0,0 +1,42 @@
+package com.example.taller3
+
+
+import android.os.Parcel
+import android.os.Parcelable
+
+class Veterinario {
+    var foto: String? = null,
+    var nombre: String? = null,
+    var apellido: String? = null,
+    var estado: String? = null,
+    var uid : String? = null
+}: Parcelable {
+
+    constructor(parcel: Parcel) : this(
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString()
+    )
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(foto)
+        parcel.writeString(nombre)
+        parcel.writeString(apellido)
+        parcel.writeString(estado)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Usuario> {
+        override fun createFromParcel(parcel: Parcel): Usuario {
+            return Usuario(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Usuario?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
