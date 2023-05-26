9dcf1331d11b7f0254d66e80c651ba2759a63f69
MarcoAValencia <60183467+MarcoAValencia@users.noreply.github.com>
2023-05-26 12:12:53 -0500

    Se arreglo navegacion

--- app/src/main/java/com/example/taller3/TipoUsuarioActivity.kt
+++ app/src/main/java/com/example/taller3/TipoUsuarioActivity.kt
@@ -1,11 +1,27 @@
 package com.example.taller3
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.taller3.databinding.ActivityTipoUsuarioBinding
+import com.example.taller3.databinding.LoginBinding
 
 class TipoUsuarioActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityTipoUsuarioBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_tipo_usuario)
+        binding = ActivityTipoUsuarioBinding.inflate(layoutInflater)
+
+        binding.buttonUsuario.setOnClickListener {
+            val intent = Intent(this, RegistroDatos::class.java)
+            startActivity(intent)
+        }
+        binding.buttonVeterinario.setOnClickListener {
+            val intent = Intent(this, RegistroDatosVetActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
